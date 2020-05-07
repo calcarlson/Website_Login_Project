33d640a499c8604ca5d68435094a2359764ddb38
calcarlson <calcarlson@gmail.com>
2020-05-07 18:45:36 -0500

    damn }

--- HW7/carl5362_hw07/create_accounts_table.js
+++ HW7/carl5362_hw07/create_accounts_table.js
@@ -22,7 +22,7 @@ con.connect(function(err) {
     var sql = `CREATE TABLE tbl_accounts(acc_id INT NOT NULL AUTO_INCREMENT,
                                        acc_name VARCHAR(20),
                                        acc_login VARCHAR(20),
-                                       acc_password VARCHAR(50), PRIMARY KEY (acc_id))`;
+                                       acc_password VARCHAR(50), `;
     con.query(sql, function(err, result) {
         if (err) {
             throw err;
